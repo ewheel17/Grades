8fadf710ab2d9967df3e5d9cf9e4b024f0c57ec0
Max <ewheel17@gmail.com>
2018-05-30 12:09:12 -0600

    Full desktop app addition

---
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class TypeTests
+    {
+        //Arrays are Reference Types, so when they are passed into a method
+        //they can and will change any referenced variables in memory.
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        //The string type is a Reference Type that behaves like a Value Type.
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "max";
+            name = name.ToUpper();
+
+            Assert.AreEqual("MAX", name);
+        }
+
+        //Immutability proofing. Just using AddDays(1) will not change the value.
+        //Typing "date =" assigns the new value to a variable.
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        //The Value Type variable being passed into the method is not being
+        //changed when the variable copy is changed.
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number += 1;
+        }
+
+        //The pointer in a reference type is passed into the method.
+        //The object in memory being pointed to can be changed.
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book2.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
+        public void StringComparisons()
+        {
+            string name1 = "Max";
+            string name2 = "max";
+
+            bool result = String.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
---
+++ Grades/GradeBook.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook : GradeTracker
+    {
+        //Default constructor (no parameters).
+        //Constructors are the only methods where you do not specify a return type.
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public override void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            Console.WriteLine("GradeBook::ComputeStatistics");
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
+
+        //The protected value allows access to the List in this class, or a derived class.
+        //It is still private in the sense that it cannot be accessed outside.
+        protected List<float> grades;
+    }
+}
